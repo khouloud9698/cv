271f9e4fa65b674995674ba3e9bb24ee2a95b453
khouloud hamdi <khouloudhamdi48@gmail.com>
2020-11-09 16:25:05 +0100

    english cv

--- components/Introduction.js
+++ components/Introduction.js
@@ -6,14 +6,14 @@ export default () => (
         Hello! I&#39;m Khouloud Hamdi, a computer engineering student at Higher Institute of Computer Science (ISI)
       </p>
       <p className="intro-p">
-        I am looking for end of studies internship starting from Februay 2021
+        I am looking for end of studies internship starting from February 2021
       </p>
       <span className="intro-p job-pls">
 	  In fact, i am always open to all offers and motivated to learn
-	  about new fields and technologies. But here ara some suggested fields: 
+	  about new fields and technologies. But here are some suggested fields: 
 	  
-  <p>Mobile developement</p>
-  <p>Web developement</p>
+  <p>Mobile development</p>
+  <p>Web development</p>
   <p>Big data...</p>
     
         		      	
