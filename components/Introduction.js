b649d83c87ff1b3792dda0a965f5ea227f24baac
khouloud hamdi <khouloudhamdi48@gmail.com>
2020-11-10 16:54:45 +0100

    school

--- components/Introduction.js
+++ components/Introduction.js
@@ -3,14 +3,14 @@ export default () => (
     <h4>Intro</h4>
     <div className="content">
       <p className="intro-p">
-        Hello! I&#39;m Khouloud Hamdi, a computer engineering student at Higher Institute of Computer Science (ISI)
+        Hello! I&#39;m Khouloud HAMDI, a computer engineering student at Higher Institute of Computer Science (ISI)
       </p>
       <p className="intro-p">
         I am looking for end of studies internship starting from February 2021
       </p>
       <span className="intro-p job-pls">
 	  In fact, i am always open to all offers and motivated to learn
-	  about new fields and technologies. But here are some suggested fields: 
+	  about new fields and technologies. But, here are some suggested fields: 
 	  
   <p>Mobile development</p>
   <p>Web development</p>
